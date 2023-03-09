409a47b68b0657355157244eca791a467e3b0240
friadyaseen <62214606+friadyaseen@users.noreply.github.com>
2023-03-09 15:46:58 +0300

    Add files via upload

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,7 +1,4 @@
-
-//make it more biutiflul
-//responsivness
-
+//get location
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(GetWeather);
 } else {
@@ -9,43 +6,44 @@ if (navigator.geolocation) {
 }
 
 
-
+//get data
 function GetWeather(positon) {
   lat = Math.round(positon.coords.latitude);
   lon = Math.round(positon.coords.longitude);
 
+  //get current weather data
   fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=d683d4ce2bb76c53f54bda835a48260c")
     .then(response =>
       response.json().then(data => ({
         data: data,
         status: response.status
-      })
-      ).then(res => {
+      })).then(res => {
         GetCurrentWeather(res.data)
       }));
 
-
+  //get weather forcast data
   fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=d683d4ce2bb76c53f54bda835a48260c")
     .then(response =>
       response.json().then(data => ({
         data: data,
         status: response.status
-      })
-      ).then(res => {
+      })).then(res => {
         SetWeakTemp(res.data)
       }));
 
+  //get country and city name
   fetch("https://us1.locationiq.com/v1/reverse?key=pk.c006302e4e7295adba0bed3dfdf5297b&lat=" + positon.coords.latitude + "&lon=" + positon.coords.longitude + "&format=json")
     .then(response =>
       response.json().then(data => ({
         data: data,
         status: response.status
-      })
-      ).then(res => {
+      })).then(res => {
         Getcityname(res.data)
       }));
 }
 
+
+//set current weather data to html
 function GetCurrentWeather(currentweather) {
 
   let tempnaw = Math.round(currentweather.main.temp - 273);
@@ -67,7 +65,7 @@ function GetCurrentWeather(currentweather) {
 }
 
 
-
+//set weather forcast data to html
 function SetWeakTemp(weatherFor) {
   let j = 1;
   for (i = 0; i < weatherFor.list.length; i++) {
@@ -82,6 +80,8 @@ function SetWeakTemp(weatherFor) {
   }
 }
 
+
+//turn number to days of the week
 function Getdate(i, weatherFor) {
   let date = new Date(weatherFor.list[i].dt * 1000);
   switch (date.getDay()) {
@@ -102,28 +102,32 @@ function Getdate(i, weatherFor) {
   }
 }
 
+
+//remove n and d for icon names from data
 function GetIcon(i, weatherFor) {
   let j = weatherFor.list[i].weather[0].icon.replace(/(n)|(d)/, '')
   return (j);
 }
 
+//remove governorate eh
 function Getcityname(file) {
   document.getElementById("country").innerText = file.address.country;
   document.getElementById("city").innerText = file.address.state.replace("Governorate", "");
 }
 
 let hello = false;
-
+//change for C to F
 function changedegree() {
   hello = !hello;
   if (hello) {
     tofahrenheit();
-  }
-  else {
+  } else {
     tocelsius()
   }
 }
 
+
+// set C to F
 function tofahrenheit() {
   for (i = 1; i < 6; i++) {
     document.getElementById('temp' + i).innerText = Math.round((document.getElementById('temp' + i).innerText) * (9 / 5) + 32);
@@ -139,6 +143,8 @@ function tofahrenheit() {
   document.getElementById('degreea').innerHTML = "&#8457;";
 }
 
+
+//Set F to C
 function tocelsius() {
   for (i = 1; i < 6; i++) {
     document.getElementById('temp' + i).innerText = Math.round(((document.getElementById('temp' + i).innerText) - 32) * (5 / 9));
